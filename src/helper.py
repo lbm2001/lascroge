71ddd2a3c8d427d5aa449fa8d5fad26a4c547f4e
Lukas Müller <lukasmueller@Mac.fritz.box>
2025-07-10 21:38:48 +0200

    Added separate file for the encoder

---
+++ src/helper.py
@@ -0,0 +1,104 @@
+import torch
+from torch.autograd import Variable
+import torch.nn as nn 
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class TreeNode(object):  
+
+    def __init__(self, attr): 
+
+        self.neighbors = []  
+        self.features = attr 
+        
+    def add_neighbor(self, nei_node):  
+        self.neighbors.append(nei_node)  
+
+class ModTree(object):  
+
+    def __init__(self, attr, conn):  
+        self.nodes = []  
+        for i,c in enumerate(attr):  
+            node = TreeNode(c)  
+            self.nodes.append(node)  
+
+        for i in range(len(attr)): 
+            for j in range(i + 1, len(attr)): 
+                if conn[i, j] != 0: 
+                    self.nodes[i].add_neighbor(self.nodes[j])  
+                    self.nodes[j].add_neighbor(self.nodes[i]) 
+
+
+        for i,node in enumerate(self.nodes): 
+            node.nid = i + 1 
+            node.is_leaf = (len(node.neighbors) == 1)  
+
+    def size(self): 
+        return len(self.nodes) 
+
+def GRU(x, h_nei, W_z, W_r, U_r, W_h):
+    hidden_size = x.size()[-1]
+    sum_h = h_nei.sum(dim=1)
+    z_input = torch.cat([x,sum_h], dim=1)
+    z = torch.sigmoid(W_z(z_input))
+
+    r_1 = W_r(x).view(-1,1,hidden_size)
+    r_2 = U_r(h_nei)
+    r = torch.sigmoid(r_1 + r_2)
+    
+    gated_h = r * h_nei
+    sum_gated_h = gated_h.sum(dim=1)
+    h_input = torch.cat([x,sum_gated_h], dim=1)
+    pre_h = torch.tanh(W_h(h_input))
+    new_h = (1.0 - z) * sum_h + z * pre_h
+    return new_h
+
+def create_var_float(tensor, requires_grad=False):
+    return Variable(tensor.float().to(device), requires_grad=requires_grad)
+
+def create_var_int(tensor, requires_grad=False):
+    return Variable(tensor.to(device), requires_grad=requires_grad)
+
+def index_select_ND(source, dim, index):
+    index_size = index.size()
+    suffix_dim = source.size()[1:]
+    final_size = index_size + suffix_dim
+    target = source.index_select(dim, index.view(-1))
+    return target.view(final_size)
+
+
+class GraphGRU(nn.Module):
+
+    def __init__(self, input_size, hidden_size, depth):
+        super(GraphGRU, self).__init__()
+        self.hidden_size = hidden_size
+        self.input_size = input_size
+        self.depth = depth
+
+        self.W_z = nn.Linear(input_size + hidden_size, hidden_size)
+        self.W_r = nn.Linear(input_size, hidden_size, bias=False)
+        self.U_r = nn.Linear(hidden_size, hidden_size)
+        self.W_h = nn.Linear(input_size + hidden_size, hidden_size)
+
+    def forward(self, h, x, mess_graph):
+        mask = torch.ones(h.size(0), 1)
+        mask[0] = 0 
+        mask = create_var_int(mask)
+        for it in range(self.depth):
+            h_nei = index_select_ND(h, 0, mess_graph)
+            sum_h = h_nei.sum(dim=1)
+            z_input = torch.cat([x, sum_h], dim=1)
+            z = torch.sigmoid(self.W_z(z_input))
+
+            r_1 = self.W_r(x).view(-1, 1, self.hidden_size)
+            r_2 = self.U_r(h_nei)
+            r = torch.sigmoid(r_1 + r_2)
+            
+            gated_h = r * h_nei
+            sum_gated_h = gated_h.sum(dim=1)
+            h_input = torch.cat([x, sum_gated_h], dim=1)
+            pre_h = torch.tanh(self.W_h(h_input))
+            h = (1.0 - z) * sum_h + z * pre_h
+            h = h * mask
+
+        return h
